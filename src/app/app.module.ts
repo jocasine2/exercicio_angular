e9be8df4e3f98a540cb708084646005eb0b9d32d
Joaquim Campelo da Silva Neto <jcsnetcc@gmail.com>
2022-01-17 01:08:19 -0300

    ✨ exercícios Angualar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormLoginComponent } from './shared/components/form-login/form-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormLoginComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/shared/components/form-login/form-login.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-form-login',
+  templateUrl: './form-login.component.html',
+  styleUrls: ['./form-login.component.css']
+})
+export class FormLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  checkCreditCard(){
+    console.log(this)
+  }
+
+}
