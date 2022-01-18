dd71797217c546b82eaf5cf9cec633d8cd976814
Joaquim Campelo da Silva Neto <jcsnetcc@gmail.com>
2022-01-18 01:34:45 -0300

    adiciona evento que seta imagem

--- src/app/shared/components/form-login/form-login.component.ts
+++ src/app/shared/components/form-login/form-login.component.ts
@@ -6,14 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./form-login.component.css']
 })
 export class FormLoginComponent implements OnInit {
-
+  displayVal = '';
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  checkCreditCard(){
-    console.log(this)
-  }
+  checkCreditCard(numero:string){
+    console.log(numero);
 
+    switch (numero) {
+      case '37':
+        this.displayVal = '<img class="img img-responsive" style="width: 50px;" src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg">';
+      break;
+      case '4':
+        this.displayVal = '<img class="img img-responsive" style="width: 50px;" src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg">';
+      break;
+      case '5':
+        this.displayVal = '<img class="img img-responsive" style="width: 50px;" src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg">';
+      break;
+      case '':
+        this.displayVal = '';
+      break;
+      default:
+      break;
+    }
+    
+  }
 }
